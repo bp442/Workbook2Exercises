4623920b97f4271840dfa54d41661bce33124ffb
bp442 <bpark135@gmail.com>
2023-10-19 09:13:08 -0400

    fixed layout of rule_of_72

--- MathScripts/rule_of_72.js
+++ MathScripts/rule_of_72.js
@@ -2,9 +2,11 @@
 //sample worth is 500000, sample interest rate is 5%
 
 var currentWorth = 500000;
-var interestRate = 5;
+
+var interestRate = 5; //Whole number %
 
 var time = 72 / interestRate;
 
-console.log("At a " + interestRate + "% interest rate, your savings account of $" + 
-currentWorth.toFixed(2) + " will be worth $" + (currentWorth * 2).toFixed(2) + " in " + time.toFixed(1) + " years.");
+console.log("At a " + interestRate + "% interest rate, your savings account of $" +
+    currentWorth.toFixed(2) + " will be worth $" + (currentWorth * 2).toFixed(2) + 
+    " in " + time.toFixed(1) + " years.");
