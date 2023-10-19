f20e32d2235c65f63c7bc08a42cfc4bfb7af20df
bp442 <bpark135@gmail.com>
2023-10-19 09:07:53 -0400

    added conversion_tests on page 2-33

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,62 @@
+//Description:  This script tests various numeric
+//              conversion techniques
+//Author: Brian Park
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+
+
+/* Question 1 Comment Block
+//Convert each using parseInt() and display the results
+a = parseInt(a);
+b = parseInt(b);
+c = parseInt(c);
+d = parseInt(d);
+
+console.log("a= " + a); //returns 101
+console.log("b= " + b); //returns 55
+console.log("c= " + c); //returns 402
+console.log("d= " + d); //returns NaN
+*/
+
+/* Question 2 Comment Block
+//Convert each using parseFloat() and display the results
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+d = parseFloat(d);
+
+console.log("a= " + a); //returns 101.1
+console.log("b= " + b); //returns 55
+console.log("c= " + c); //returns 402
+console.log("d= " + d); //returns NaN
+*/
+
+/* Question 3 Comment Block
+//Convert each using Number() and display the results
+a = Number(a);
+b = Number(b);
+c = Number(c);
+d = Number(d);
+
+console.log("a= " + a); //returns 101.1
+console.log("b= " + b); //returns 55
+console.log("c= " + c); //returns NaN
+console.log("d= " + d); //returns NaN
+*/
+
+/* Question 4 Comment Block
+//Convert each using the unary + operator and display the results
+a = +a;
+b = +b;
+c = +c;
+d = +d;
+
+console.log("a= " + a); //returns 101.1
+console.log("b= " + b); //returns 55
+console.log("c= " + c); //returns NaN
+console.log("d= " + d); //returns NaN
+*/
